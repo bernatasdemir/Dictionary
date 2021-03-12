685bd35f45a48e9cf9ac664c4dff5dda267fe98e
BERNA NUR TAŞDEMİR <17050211051@ybu.edu.tr>
2021-03-12 23:08:17 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        public TKey[] keys;
+        public TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArray1 = keys;
+            TValue[] tempArray2 = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                values[i] = tempArray2[i];
+            }
+            values[values.Length - 1] = value;
+
+        }
+        public int Count()
+        {
+            return keys.Length;
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> flowers = new MyDictionary<int, string>();
+            flowers.Add(1, "Gül");
+            flowers.Add(2, "Zambak");
+            flowers.Add(3, "Kiraz Çiçeği");
+            flowers.Add(4, "Şakayık");
+            flowers.Add(5, "Lavanta");
+            flowers.Add(6, "Orkide");
+            flowers.Add(7, "Manolya");
+            flowers.Add(8, "Zambak");
+            flowers.Add(9, "Yasemin");
+            flowers.Add(10, "Sümbül");
+
+            foreach (var flower in flowers.values)
+            {
+                Console.WriteLine(flower);
+            }
+        }
+    }
+}
